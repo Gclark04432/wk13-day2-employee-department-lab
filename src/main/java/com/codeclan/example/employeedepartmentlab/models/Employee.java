89a28528836d9bd7d45d3b0d606d1d674976f6d3
Gary Clark <gclark04432@live.co.uk>
2020-02-25 10:57:03 +0000

    add model and repository for employee and department

--- src/main/java/com/codeclan/example/employeedepartmentlab/models/Employee.java
+++ src/main/java/com/codeclan/example/employeedepartmentlab/models/Employee.java
@@ -1,4 +1,62 @@
 package com.codeclan.example.employeedepartmentlab.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String employeeNumber;
+
+    public Employee(Long id, String firstName, String lastName, String employeeNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employeeNumber = employeeNumber;
+    }
+
+    public Employee() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(String employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
 }
