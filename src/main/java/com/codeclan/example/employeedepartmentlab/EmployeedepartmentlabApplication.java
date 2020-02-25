c644237beff290d75631aa882c1deb121f9b9f2c
Gary Clark <gclark04432@live.co.uk>
2020-02-25 10:47:09 +0000

    create spring project outline

---
+++ src/main/java/com/codeclan/example/employeedepartmentlab/EmployeedepartmentlabApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.employeedepartmentlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeedepartmentlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeedepartmentlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/employeedepartmentlab/EmployeedepartmentlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.employeedepartmentlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeedepartmentlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
